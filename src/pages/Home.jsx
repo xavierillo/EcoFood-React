8210c8f54f17e6ecced610a88988e38dad8dfaf8
Laboratorio Computación N° 1 <claseslab1@inacap.cl>
2025-05-07 10:46:33 -0400

    detalle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import CardProducto from '../components/CardProducto';
 function Home() {
   return (
     <div className="container mt-4">
-        <h1>Productos Disponibles</h1>
+        <h1>Productos Disponibles Prueba </h1>
         <CardProducto nombre="Pan Integral" precio="$500" />
         <CardProducto nombre="Pan normal" precio="$300" />
     </div>
